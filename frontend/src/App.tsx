2fccf16afdcde5e5de006ff1eb9d583231f43fd3
Mohamed el aissaoui <shigara2019@gmail.com>
2026-08-27 15:10:25 +0000

    React Router installation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<div>Home Page</div>}/>
-      <Route path="/login" element={<div>Login Page</div>}/>
+      <Route path="/" element={<HomePage/>}/>
     </Routes>
     </BrowserRouter>
   )
